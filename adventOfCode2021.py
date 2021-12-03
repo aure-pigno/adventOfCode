c6a0012272156384404fe74092f6149d1780a63e
aure-pigno <aurelien.pignolet@gmail.com>
2021-12-03 18:16:42 +0100

    Update adventOfCode2021.py

--- adventOfCode2021.py
+++ adventOfCode2021.py
@@ -25,3 +25,21 @@ def exec_2021day2():
     text_table = f.split("\n")
     print("Part 1:", f_2021day2(text_table))
     print("Part 2:", f_2021day2(text_table, 3))
+
+def f_2021day3(text_table, part=1):
+    if part == 1:
+        gamma, epsilon = "", ""
+        for i in range(0, len(text_table[0])):
+            elem = Helper.f_2021day3_getBitMap(i, text_table)
+            v = int(len(elem["1"]) > len(elem["0"]))
+            epsilon += str(v)
+            gamma += str(1-v)
+        return int(epsilon, 2)*int(gamma, 2)
+
+    return Helper.f_2021day3_getRecord(text_table) * Helper.f_2021day3_getRecord(text_table, False)
+
+def exec_2021day3():
+    f = open("input/Input_2021day3.txt", "r").read()
+    text_table = f.split("\n")
+    print("Part 1:", f_2021day3(text_table))
+    print("Part 2:", f_2021day3(text_table, 2))
