ab042b7e99d89aacc28341993228cf63cb481c51
Aurelien <aurelien.pignolet@gmail.com>
2021-12-06 08:03:16 +0100

    Day 6

--- AOCSolver_2021_6.py
+++ AOCSolver_2021_6.py
@@ -1,11 +1,31 @@
 import helper
+from datetime import datetime
 from AOCSolver import AOCSolver
 
 
 class AOCSolver_2021_6(AOCSolver):
 
     def parse(self, input):
-        self.table = input.split("\n")
+        self.table = helper.table_converter(input.replace("\n", "").split(","))
 
-    def execute(self, part=1):
-        return part
+    def execute(self, it=1, repro = 6, mx = 8):
+        lst = [0 for i in range(mx + 1)]
+        for elem in self.table:
+            lst[elem] += 1
+        for l in range(0, it):
+            self.slide(lst, repro, mx)
+        return sum(lst)
+
+    @staticmethod
+    def slide(lst, repro, mx):
+        temp = lst[0]
+        for i in range(0, mx):
+            lst[i] = lst[i + 1]
+        lst[mx] = temp
+        lst[repro] = lst[repro] + lst[mx]
+
+    def part1(self):
+        return self.execute(it=80)
+
+    def part2(self):
+        return self.execute(it=256)
--- main.py
+++ main.py
@@ -82,5 +82,5 @@ def execute(n):
     solver.solve(2)
 
 
-for i in range(1,26):
+for i in range(6,7):
     execute(i)
