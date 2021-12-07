2a39ae95c1620401606d6a85468bc03bc2fe8482
Aurelien <aurelien.pignolet@gmail.com>
2021-12-07 08:21:52 +0100

    use delta around average

--- AOCSolver_2021_7.py
+++ AOCSolver_2021_7.py
@@ -8,5 +8,9 @@ class AOCSolver_2021_7(AOCSolver):
         self.table = helper.table_converter(input.replace("\n", "").split(","))
 
     def execute(self, part=1):
-        p = round(helper.median(self.table)) if part == 1 else int(helper.average(self.table))
-        return sum([int(abs(p - elem)) if part == 1 else int(abs(p - elem)) * int(1 + abs(p - elem))/2 for elem in self.table])
+        p1 = int(helper.median(self.table)) if part == 1 else int(helper.average(self.table))
+        pr1, pr2, p2 = 0, 0, p1 + 1
+        for elem in self.table:
+            pr1 += abs(p1 - elem) if part == 1 else int(abs(p1 - elem) * (1 + abs(p1 - elem)) / 2)
+            pr2 += abs(p2 - elem) if part == 1 else int(abs(p2 - elem) * (1 + abs(p2 - elem)) / 2)
+        return min(pr1, pr2)
