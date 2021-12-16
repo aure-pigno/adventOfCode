b3dbf17c7d0dca16326e80e32da2d94c29fd456a
Aurelien <aurelien.pignolet@gmail.com>
2021-12-16 10:10:31 +0100

    Correct param

--- AOCSolver_2021_16.py
+++ AOCSolver_2021_16.py
@@ -9,7 +9,7 @@ class AOCSolver_2021_16(AOCSolver):
     v_map = {"0": "0000", "1": "0001", "2": "0010", "3": "0011", "4": "0100", "5": "0101", "6": "0110", "7": "0111", "8": "1000", "9": "1001", "A": "1010", "B": "1011", "C": "1100", "D": "1101", "E": "1110", "F": "1111"}
 
     def parse(self, input):
-        self.binary_data = "".join([self.v_map[v] for v in helper.split(data)])
+        self.binary_data = "".join([self.v_map[v] for v in helper.split(input)])
         self.pack = self.parse_packet(0)[0]
 
     def execute(self, part=1):
